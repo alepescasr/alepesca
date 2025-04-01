cd8ece2ea5c9dc0ad1e2a3062d38e542d9924ed7
Ciro <metralciro@gmail.com>
2025-04-01 06:57:00 -0300

    no cache en los get

--- actions/get-product.tsx
+++ actions/get-product.tsx
@@ -3,9 +3,16 @@ import { Product } from "@/types";
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 const getProduct = async (id: string): Promise<Product> => {
-  const res = await fetch(`${URL}/${id}`);
+  const res = await fetch(`${URL}/${id}`, 
+    {
+      cache: 'no-store',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      }
+    }
+  );
   console.log("toma los productos???" + res);
-
   return res.json();
 };
 
