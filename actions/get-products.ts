5fd01510b9ea8fcc2b73b0a3a8bd2dcda38c0893
ginopastran <ginopastran@gmail.com>
2025-04-01 20:49:16 -0300

    -Trying to fix errors

--- actions/get-products.ts
+++ actions/get-products.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
@@ -7,20 +7,20 @@ export interface Query {
   colorId?: string;
   sizeId?: string;
   isFeatured?: boolean;
+  hasOffer?: boolean;
 }
 
 const getProducts = async (query: Query): Promise<any[]> => {
   try {
     const response = await axios.get(URL, {
       params: query,
-      cache: 'no-store'
     });
 
     return response.data;
   } catch (error) {
-    console.error('Error al obtener productos:', error);
+    console.error("Error al obtener productos:", error);
     return [];
   }
 };
 
-export default getProducts; 
+export default getProducts;
